1ec79e601176cb956d7344c6ecadc8823682034d
Bruno Silva <brunomiguelsilva@ua.pt>
2015-10-29 00:12:35 +0000

    added models for users, players, devices

--- m1/server/users_data/models.py
+++ m1/server/users_data/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from books.models import Book
+from player.models import Player
 
 # Create your models here.
 
 class User_key(models.Model):
     user = models.OneToOneField(User)
-    user_key = models.BinaryField(max_length=200)
-
+    user_key = models.BinaryField(max_length=128)
+    player_used = models.ForeignKey(Player, null=True)
 
 class Devices(models.Model):
     user = models.ManyToManyField(User)
-    device_key = models.BinaryField(max_length=200)
+    device_key = models.BinaryField(max_length=128)
+    device_name = models.CharField(max_length=250)
+
+class Purchases(models.Model):
+    user = models.ForeignKey(User)
+    book_id = models.ForeignKey(Book)
+    random = models.BinaryField(max_length=128)
+    file_key = models.BinaryField(max_length=128)
