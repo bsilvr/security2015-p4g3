5b1344d62bdd80d304c9a3745b355531779f6610
Bruno Silva <brunomiguelsilva@ua.pt>
2015-10-29 00:13:34 +0000

    urls added, started views in users and new migrations

--- m1/server/users_data/migrations/0001_initial.py
+++ m1/server/users_data/migrations/0001_initial.py
@@ -9,6 +9,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('player', '0001_initial'),
+        ('books', '0003_volume_volume_number'),
     ]
 
     operations = [
@@ -16,15 +18,27 @@ class Migration(migrations.Migration):
             name='Devices',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('device_key', models.BinaryField(max_length=200)),
+                ('device_key', models.BinaryField(max_length=128)),
+                ('device_name', models.CharField(max_length=250)),
                 ('user', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Purchases',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('random', models.BinaryField(max_length=128)),
+                ('file_key', models.BinaryField(max_length=128)),
+                ('book_id', models.ForeignKey(to='books.Book')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='User_key',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('user_key', models.BinaryField(max_length=200)),
+                ('user_key', models.BinaryField(max_length=128)),
+                ('player_used', models.ForeignKey(to='player.Player', null=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
