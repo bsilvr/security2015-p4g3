459210318e13a755427f3db59d2a0a1266bf16a7
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-09 17:42:20 +0000

    removed end point

--- m1/server/client_requests/views.py
+++ m1/server/client_requests/views.py
@@ -12,54 +12,54 @@ from player.models import Player
 from restrictions.models import *
 
 
-@api_view(['POST'])
-def read_book(request):
-    book_id = request.POST.get('book_id')
-    # email = request.POST.get('user')
-    #
-    # if email==None or email=="":
-    #     return HttpResponse("Invalid email", status=status.HTTP_400_BAD_REQUEST)
-    #
-    # user = User.objects.get(email=email)
-    #
-    # if user==None:
-    #     return HttpResponse("User doesn't exist", status=status.HTTP_400_BAD_REQUEST)
-
-    if not request.user.is_authenticated():
-        return HttpResponse("User not logged in", status=status.HTTP_403_FORBIDDEN)
-
-
-    if book_id==None or book_id=="":
-        return HttpResponse("Invalid book_id", status=status.HTTP_400_BAD_REQUEST)
-
-    book = Book.objects.get(ebook_id=book_id)
-
-    if book is None:
-        return HttpResponse("Book doesn't exists", status=status.HTTP_400_BAD_REQUEST)
-
-    p = Purchases.objects.all().filter(user=request.user, book_id=book)
-
-    if len(p) == 0:
-        return HttpResponse("User hasn't bought the book", status=status.HTTP_403_FORBIDDEN)
-
-
-    restrictions = {
-                    "book_id":"",
-                    "device_key":"",
-                    "player_key":"",
-                    "location":"",
-                    "so":"",
-                    "time":"",
-                    }
-
-    return HttpResponse(json.dumps(restrictions), status=status.HTTP_200_OK)
+# @api_view(['POST'])
+# def read_book(request):
+#     book_id = request.POST.get('book_id')
+#     # email = request.POST.get('user')
+#     #
+#     # if email==None or email=="":
+#     #     return HttpResponse("Invalid email", status=status.HTTP_400_BAD_REQUEST)
+#     #
+#     # user = User.objects.get(email=email)
+#     #
+#     # if user==None:
+#     #     return HttpResponse("User doesn't exist", status=status.HTTP_400_BAD_REQUEST)
+#
+#     if not request.user.is_authenticated():
+#         return HttpResponse("User not logged in", status=status.HTTP_403_FORBIDDEN)
+#
+#
+#     if book_id==None or book_id=="":
+#         return HttpResponse("Invalid book_id", status=status.HTTP_400_BAD_REQUEST)
+#
+#     book = Book.objects.get(ebook_id=book_id)
+#
+#     if book is None:
+#         return HttpResponse("Book doesn't exists", status=status.HTTP_400_BAD_REQUEST)
+#
+#     p = Purchases.objects.all().filter(user=request.user, book_id=book)
+#
+#     if len(p) == 0:
+#         return HttpResponse("User hasn't bought the book", status=status.HTTP_403_FORBIDDEN)
+#
+#
+#     restrictions = {
+#                     "book_id":"",
+#                     "device_key":"",
+#                     "player_key":"",
+#                     "location":"",
+#                     "so":"",
+#                     "time":"",
+#                     }
+#
+#     return HttpResponse(json.dumps(restrictions), status=status.HTTP_200_OK)
 
 
 # consulted code from: http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto
 # consulted code from http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-256
 
 @api_view(['POST'])
-def validate(request):
+def read_book(request):
     book_id = request.POST.get('book_id')
     device_key = request.POST.get('device_key')
     player_key = request.POST.get('player_key')
@@ -80,6 +80,12 @@ def validate(request):
 
     if book is None:
         return HttpResponse("Book doesn't exists", status=status.HTTP_400_BAD_REQUEST)
+
+    p = Purchases.objects.all().filter(user=request.user, book_id=book)
+
+    if len(p) == 0:
+        return HttpResponse("User hasn't bought the book", status=status.HTTP_403_FORBIDDEN)
+
 
 
     if device_key==None or device_key=="":
