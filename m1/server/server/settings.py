b44566b0ec55ebe4c2385ba5bc1ff56d756a98e3
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-12 12:19:08 +0000

    added https

--- m1/server/server/settings.py
+++ m1/server/server/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = (
     'player',
     'users_data',
     'restrictions',
-    'client_requests'
+    'client_requests',
+    'django_extensions' # because of ssl. adapted from link: http://stackoverflow.com/questions/8023126/how-can-i-test-https-connections-with-django-as-easily-as-i-can-non-https-connec
 
 )
 
