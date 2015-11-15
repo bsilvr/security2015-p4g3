ab93eb1b87fab1cd1b89dfac385d6a873d4f821d
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-15 17:20:25 +0000

    updated install.sh

--- m1/server/install.sh
+++ m1/server/install.sh
@@ -4,22 +4,26 @@
 #
 #
 # usage: ./install.sh
-printf "Intalling python-pip..."
+printf "Intalling python-pip...\n"
 sudo apt-get install python-pip
 printf "done\n"
 
-printf "Intalling Django..."
+printf "Intalling Django...\n"
 pip install Django==1.8.6
 printf "done\n"
 
-printf "Installing library dependencies..."
-pip install pycrypto django-extensions Werkzeug pyOpenSSL
+printf "Installing library dependencies...\n"
+pip install pycrypto django-extensions Werkzeug pyOpenSSL djangorestframework
 printf "done\n"
 
-printf "Creating database..."
+printf "Removing previous databases...\n"
+rm db.sqlite3
+printf "done\n"
+
+printf "Creating database...\n"
 python manage.py migrate
 printf "done\n"
 
-printf "Populating database..."
+printf "Populating database...\n"
 python db_scripts.py
 printf "done\n"
