b38720a5c91c7400c0a7eeb6b078b3a73f85231f
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-15 17:05:33 +0000

    script to install Django server, database and populate database

---
+++ m1/server/install.sh
@@ -0,0 +1,22 @@
+#
+#
+# Script to install all needed libraries for execution
+#
+#
+# usage: ./install.sh
+
+printf "Intalling Django..."
+pip install Django==1.8.6
+printf "done\n"
+
+printf "Installing library dependencies..."
+pip install pycrypto django-extensions Werkzeug pyOpenSSL
+printf "done\n"
+
+printf "Creating database..."
+python manage.py migrate
+printf "done\n"
+
+printf "Populating database..."
+python db_scripts.py
+printf "done\n"
