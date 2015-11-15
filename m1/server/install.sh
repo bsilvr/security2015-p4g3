5863512169b6d89e3a8eda95c75d0097c1fd9fc7
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-15 18:04:28 +0000

    removed sudo from usage

--- m1/server/install.sh
+++ m1/server/install.sh
@@ -3,7 +3,7 @@
 # Script to install all needed libraries for execution
 #
 #
-# usage: sudo sh install.sh
+# usage: sh install.sh
 printf "Intalling python-pip...\n"
 sudo apt-get install python-pip
 printf "done\n"
