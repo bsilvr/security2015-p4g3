dafca91afb356ad18c5e398c7e5aedf17c66b901
Bruno Silva <brunomiguelsilva@ua.pt>
2015-11-15 18:04:53 +0000

    added https to image urls

--- m1/server/db_scripts.py
+++ m1/server/db_scripts.py
@@ -6,15 +6,15 @@ from restrictions.models import Countries, OperatingSystem
 from player.models import Player
 
 
-b0 = Book(author="Elliot, Frances", title="Old Court Life in Spain", language="English", cover="http://i.imgur.com/DIvCaPV.jpg", ebook_id=49941, text_file="media/books/pg49941.txt")
+b0 = Book(author="Elliot, Frances", title="Old Court Life in Spain", language="English", cover="https://i.imgur.com/DIvCaPV.jpg", ebook_id=49941, text_file="media/books/pg49941.txt")
 
-b1 = Book(author="Wells, Carolyn", title="The Mystery of the Sycamore", language="English", cover="http://i.imgur.com/WdTJXit.jpg", ebook_id=50209, text_file="media/books/pg50209.txt")
+b1 = Book(author="Wells, Carolyn", title="The Mystery of the Sycamore", language="English", cover="https://i.imgur.com/WdTJXit.jpg", ebook_id=50209, text_file="media/books/pg50209.txt")
 
-b2 = Book(author="Bowen, Robert Sidney", title="Dave Dawson with the Pacific Fleet", language="English", cover="http://i.imgur.com/rnMUKIg.jpg", ebook_id=50217, text_file="media/books/pg50217.txt")
+b2 = Book(author="Bowen, Robert Sidney", title="Dave Dawson with the Pacific Fleet", language="English", cover="https://i.imgur.com/rnMUKIg.jpg", ebook_id=50217, text_file="media/books/pg50217.txt")
 
-b3 = Book(author="Elliot, Frances", title="Old Court Life in France", language="English", cover="http://i.imgur.com/lnObH3e.jpg", ebook_id=50218, text_file="media/books/pg50218.txt")
+b3 = Book(author="Elliot, Frances", title="Old Court Life in France", language="English", cover="https://i.imgur.com/lnObH3e.jpg", ebook_id=50218, text_file="media/books/pg50218.txt")
 
-b4 = Book(author="Various", title="The Railway Library, 1909", language="English", cover="http://imgur.com/sM6PJkM.jpg", ebook_id=50220, text_file="media/books/pg50220.txt")
+b4 = Book(author="Various", title="The Railway Library, 1909", language="English", cover="https://imgur.com/sM6PJkM.jpg", ebook_id=50220, text_file="media/books/pg50220.txt")
 
 b0.save()
 b1.save()
