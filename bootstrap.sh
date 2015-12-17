115ef5154060582ee4bcb99a1c9a4645f8a0dbb4
Bruno Silva <brunomiguelsilva@ua.pt>
2015-12-17 12:05:15 +0000

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 # Install dependencies
+echo "Install packages dependencies..."
 apt-get update
-apt-get install -y apache2 python-pip encfs
+apt-get install -y apache2 python-pip encfs libapache2-mod-wsgi
 
 pip install Django==1.8.6 djangorestframework pycrypto
 #########################################################
 
 # Copy project folder to apache directory
-echo "Coping project folder"
+echo "Coping project folder..."
 cp -r /vagrant/m2/server /var/www/
 #########################################################
 
 # Create Secure File System
+echo "Creating secure file system..."
 mkdir -p /opt/ebooks
 
 ebook = "ebookwebstore"
@@ -37,13 +39,29 @@ cp /vagrant/m2/server/media/books/* /var/www/server/media/books/
 ###########################################################
 
 # Copy apache config file
+echo "Copying apache file key..."
 cp /vagrant/m2/server/configs/apache2/sites-enabled/default-ssl.conf /etc/apache2/sites-enabled/
 ############################################################
 
 # Copy Certificates files
+echo "Copying cert files..."
 cp /vagrant/m2/server/certs/Server_CA.crt /etc/ssl/certs/
 cp /vagrant/m2/server/certs/webStore.crt /etc/ssl/private/
 cp /vagrant/m2/server/certs/webStoreKey.pem /etc/ssl/private/
+############################################################
+
+# Create database
+echo "Creating database..."
+rm /var/www/server/db.sqlite3
+python /var/www/server/manage.py migrate
+python /var/www/server/db_scripts.py
+
+chown -R www-data /var/www/server/
+chgrp -R www-data /var/www/server/
+
+chown www-data /var/www/server/db.sqlite3
+chgrp www-data /var/www/server/db.sqlite3
+
 ############################################################
 
 # Enable SSL
