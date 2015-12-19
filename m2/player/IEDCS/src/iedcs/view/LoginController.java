45b96c3f0e95732d7b11486eac24f56aefebb21d
Bernardomrf <bernardomrferreira@ua.pt>
2015-12-19 16:46:17 +0000

    login cc almost done

--- m2/player/IEDCS/src/iedcs/view/LoginController.java
+++ m2/player/IEDCS/src/iedcs/view/LoginController.java
@@ -15,10 +15,22 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-
+import sun.security.pkcs11.SunPKCS11;
+import java.security.*;
+import java.security.Provider;
+import java.security.Security;
+import java.security.KeyStore;
+import java.io.*;
+import java.util.Enumeration;
+import java.security.Signature;
+import java.util.Base64;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
 import iedcs.MainApp;
 import iedcs.model.Http_Client;
 import iedcs.model.KeyManager;
+import iedcs.model.MyCallbackHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -180,7 +192,7 @@ public class LoginController {
 		HttpPost post = new HttpPost(url);
 
 		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-		urlParameters.add(new BasicNameValuePair("email", email.getText()));
+		urlParameters.add(new BasicNameValuePair("user", email.getText()));
 
 		post.setEntity(new UrlEncodedFormEntity(urlParameters));
 
@@ -188,7 +200,8 @@ public class LoginController {
 
 		Header HeaderRandom = response.getFirstHeader("random");
 		Header HeaderTrans = response.getFirstHeader("transactionID");
-
+		//System.out.println("Random Recebido: " + HeaderRandom.getValue());
+		//System.out.println("TransId Recebido: " + HeaderTrans.getValue());
 
 		BufferedReader rd = new BufferedReader(
                 new InputStreamReader(response.getEntity().getContent()));
@@ -204,34 +217,69 @@ public class LoginController {
 			EntityUtils.consume(entity);
 		}
 
+		System.out.println("Primeira resposta: "+ response);
+
 		if(response.getStatusLine().getStatusCode()==200){
+
+			String f = "CitizenCard.cfg";
+	        Provider p = new sun.security.pkcs11.SunPKCS11(f);
+	        Security.addProvider(p);
 			KeyManager.createDeviveKey();
+			String randomSigned = "";
+			/***/
+			try{
+				KeyStore.CallbackHandlerProtection func = new KeyStore.CallbackHandlerProtection(new MyCallbackHandler());
+	            KeyStore.Builder builder = KeyStore.Builder.newInstance("PKCS11", p, func);
+	            KeyStore ks = builder.getKeyStore();
+
+	            Signature sig = Signature.getInstance("SHA1withRSA");
+
+	            sig.initSign((PrivateKey)ks.getKey("CITIZEN AUTHENTICATION CERTIFICATE", null));
+
+	            byte txt[] = Base64.getDecoder().decode(HeaderRandom.getValue());
+
+	            sig.update(txt);
+
+	            byte[] signed = sig.sign();
+
+
+	            randomSigned = Base64.getEncoder().encodeToString(signed);
+
+	            System.out.println("Random Signed: "+ randomSigned);
+
+			}catch(Exception e){
+	            e.printStackTrace();
+	            System.err.println(e);
+	            System.exit(0);
+	        }
+            /***/
+
 	    	url = Http_Client.getURL() + "users/validateLoginCC/";
 	    	String cookie="";
 
 			post = new HttpPost(url);
 
 			urlParameters = new ArrayList<NameValuePair>();
-			urlParameters.add(new BasicNameValuePair("email", email.getText()));
-			urlParameters.add(new BasicNameValuePair("password", password.getText()));
+			urlParameters.add(new BasicNameValuePair("transactionId", HeaderTrans.getValue()));
+			urlParameters.add(new BasicNameValuePair("signed", randomSigned));
 
 			post.setEntity(new UrlEncodedFormEntity(urlParameters));
 
-			HttpResponse response = Http_Client.getHttpClient().execute(post);
+			response = Http_Client.getHttpClient().execute(post);
 
 			Header[] cookies =  response.getHeaders("Set-Cookie");
 
-
-			BufferedReader rd = new BufferedReader(
+			System.out.println("Token: "+ cookies[0].getValue().substring(0, cookies[0].getValue().indexOf(";")));
+			rd = new BufferedReader(
 	                new InputStreamReader(response.getEntity().getContent()));
 
-			StringBuffer userBooks = new StringBuffer();
-			String line = "";
+			userBooks = new StringBuffer();
+			line = "";
 			while ((line = rd.readLine()) != null) {
 				userBooks.append(line);
 			}
-
-			if(response.getStatusLine().getStatusCode()==302){
+			System.out.println("Segunda resposta: "+response);
+			if(response.getStatusLine().getStatusCode()==200){
 				cookie = cookies[0].getValue().substring(0, cookies[0].getValue().indexOf(";"));/*   value.substring(value.indexOf("="),value.length()); + ";"*/
 				setCookies(cookie);
 				System.out.println(response);
@@ -243,7 +291,7 @@ public class LoginController {
 				Alert alert = new Alert(AlertType.INFORMATION);
 		        alert.setTitle("IEDCS Player");
 		        alert.setHeaderText("Login Failed");
-		        alert.setContentText("Password or email wrong");
+		        alert.setContentText("Email or CC wrong");
 		        alert.showAndWait();
 			}
 	    }
